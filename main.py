1e918dd16d520c3165d688ffb52f06b287d4e665
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-07-26 13:16:18 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('http://127.0.0.1:5000/home', code=302)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/fun')
+def fun():
+    return render_template('fun.html')
+
+@app.route('/home/branch')
+def branch():
+    return render_template('branch.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
