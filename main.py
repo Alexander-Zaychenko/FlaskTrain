022e0fd68e2b511f7cfb75a92b36329eb471809c
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-08-14 13:14:00 +0300

    fixes

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ import os
 
 app = Flask(__name__)
 
+# make dynamic page
+
+
 @app.route('/')
 def index():
     return redirect('http://127.0.0.1:5000/home', code=302)
+
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
+
 
 @app.route('/fun')
 def fun():
     return render_template('fun.html')
 
+
 @app.route('/home/branch')
 def branch():
     return render_template('branch.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
